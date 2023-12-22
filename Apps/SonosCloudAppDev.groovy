dbdcca50cbf7f9eb90632a0682f108740ae7a65c
Daniel Winks <daniel.winks@gmail.com>
2023-12-22 13:50:12 -0500

    Update SonosCloudAppDev.groovy

--- Apps/SonosCloudAppDev.groovy
+++ Apps/SonosCloudAppDev.groovy
@@ -54,9 +54,6 @@ mappings {
   path('/oauth/callback') {
   action: [ GET: 'oauthCallback' ]
   }
-  path('/events') {
-  action: [ POST: 'eventsCallback' ]
-  }
 }
 
 @Field static final String apiPrefix = 'https://api.ws.sonos.com/control/api/v1'
@@ -113,7 +110,7 @@ Map mainPage() {
       input 'logEnable', 'bool', title: 'Enable Logging', required: false, defaultValue: true
       input 'debugLogEnable', 'bool', title: 'Enable debug logging', required: false, defaultValue: false
       input 'descriptionTextEnable', 'bool', title: 'Enable descriptionText logging', required: false, defaultValue: true
-      input 'testButton', 'button', title: 'Test'
+      // input 'testButton', 'button', title: 'Test'
     }
   }
 }
@@ -149,7 +146,6 @@ Map playerPage() {
   app.removeSetting('playerDevices')
   refreshPlayersAndGroups()
   Map playerSelectionOptions = state.players.collectEntries {id, player ->  [(id): player.name]  }
-  // Map playerSelectionOptions = state.players.collectEntries { [it.id, it.name] }
   logDebug(getObjectClassName(getCurrentPlayerDevices()))
   app.updateSetting('playerDevices', [type: 'enum', value: getCurrentPlayerDevices()])
   dynamicPage(name: 'playerPage', title: 'Sonos Player Virtual Devices', nextPage: 'mainPage') {
@@ -230,17 +226,20 @@ void appButtonHandler(String buttonName) {
 
 void testButton() {
   // refreshPlayersAndGroups()
-  // createGroupDevices()
+  createGroupDevices()
   // createPlayerDevices()
   // refreshToken()
-  getAllMusicPlayers()
+  // getAllMusicPlayers()
 }
+
 void saveGroup() {
   state.userGroups[app.getSetting('newGroupName')] = [coordinatorId:app.getSetting('newGroupCoordinator'), playerIds:app.getSetting('newGroupPlayers')]
   app.removeSetting('newGroupName')
   app.removeSetting('newGroupPlayers')
   app.removeSetting('newGroupCoordinator')
   app.removeSetting('editDeleteGroup')
+
+  createGroupDevices()
 }
 
 void deleteGroup() {
@@ -248,6 +247,8 @@ void deleteGroup() {
   app.removeSetting('newGroupName')
   app.removeSetting('newGroupPlayers')
   app.removeSetting('newGroupCoordinator')
+
+  removeOrphans()
 }
 
 void cancelGroupEdit() {
@@ -399,6 +400,64 @@ void refreshTokenCallback(AsyncResponse response, Map data = null) {
   }
 }
 
+void createGroupDevices() {
+  logDebug('Creating group devices...')
+  state.userGroups.each{ it ->
+    String dni = "${app.id}-SonosGroupDevice-${it.key}"
+    DeviceWrapper device = getChildDevice(dni)
+    if (device == null) {
+      try {
+        logDebug("Creating group device for ${it.key}")
+        device = addChildDevice('dwinks', 'Sonos Cloud Group', dni, [name: 'Sonos Group', label: "Sonos Group: ${it.key}"])
+      } catch (UnknownDeviceTypeException e) {
+        logException('createGroupDevices', e)
+      }
+    }
+    String coordinatorId = it.value.coordinatorId as String
+    String playerIds =  it.value.playerIds.join(',')
+    String householdId = state.players["${it.value.coordinatorId}"].householdId as String
+    device.updateDataValue('coordinatorId', coordinatorId)
+    device.updateDataValue('playerIds', playerIds)
+    device.updateDataValue('householdId', householdId)
+  }
+  removeOrphans()
+}
+
+void createPlayerDevices() {
+  List<Map> devicesToCreate = state.players.values().findAll { it -> it.id in playerDevices } ?: []
+  logDebug(devicesToCreate)
+  for (Map player in devicesToCreate) {
+    String dni = "${app.id}-${player.id}"
+    logDebug("DNI: ${dni}")
+    DeviceWrapper device = getChildDevice(dni)
+    if (device == null) {
+      try {
+        logInfo "Creating child audio notification device for ${player.name}"
+        device = addChildDevice('dwinks', 'Sonos Cloud Player', dni, [name: 'Sonos Cloud Player', label: "Sonos Cloud - ${player.name}"])
+      } catch (UnknownDeviceTypeException e) {
+        logException 'Sonos Cloud Player driver not found (needs installing?)', e
+      }
+    }
+    player.each { key, value -> if(key != 'zoneInfo') { device.updateDataValue(key, value as String) }}
+  }
+  removeOrphans()
+}
+
+void removeOrphans() {
+  for (ChildDeviceWrapper child in app.getChildDevices()) {
+    String dni = child.getDeviceNetworkId()
+    if(child.getDataValue('id') in playerDevices) {
+      return
+    } else if(!groupsDNIs.any{it -> it.toString() == dni}) {
+      logDebug("Removing child not found in user groups: ${child}")
+      app.deleteChildDevice(dni)
+    } else {
+      logDebug("Removing child not found in selected devices list: ${child}")
+      app.deleteChildDevice(dni)
+    }
+  }
+}
+
 void getAllMusicPlayers() {
   Map params = [
     uri:"http://127.0.0.1:8080/device/listJson?capability=capability.musicPlayer",
@@ -415,37 +474,9 @@ void showMusic(AsyncResponse response, Map data = null) {
   logDebug(response.getData())
 }
 
-void componentOn(DeviceWrapper device) {
-  logDebug("componentOn()")
-  String householdId = device.getDataValue('householdId')
-  List<String> playerIds = getAllPlayersForGroupDevice(device)
-  if (householdId && playerIds) {
-    logDebug "${device} activate household ${householdId} group with players ${playerIds}"
-    Map data = ['playerIds': playerIds]
-    postJsonAsync("${apiPrefix}/households/${householdId}/groups/createGroup", data)
-  }
-  getChildDevice(device.deviceNetworkId).childParse([name:'switch', value:'on'])
-
-}
-
-void componentOff(DeviceWrapper device) {
-  logDebug "${device} off"
-  String householdId = device.getDataValue('householdId')
-  List<String> playerIds = getAllPlayersForGroupDevice(device)
-  if (householdId && playerIds) {
-    logDebug "${device} activate household ${householdId} group with players ${playerIds}"
-    playerIds.each { player ->
-      Map data = ['playerIds': [player]]
-      postJsonAsync("${apiPrefix}/households/${householdId}/groups/createGroup", data)
-    }
-  }
-  getChildDevice(device.deviceNetworkId).childParse([name:'switch', value:'off'])
-
-}
-
 List<String> getAllPlayersForGroupDevice(DeviceWrapper device) {
   List<String> playerIds = [device.getDataValue('coordinatorId')]
-  playerIds.addAll(device.getDataValue('playerIds')[1..- 2].tokenize(', '))
+  playerIds.addAll(device.getDataValue('playerIds').split(','))
   return playerIds
 }
 
@@ -554,7 +585,6 @@ void getFavoritesCallback(AsyncResponse response, Map data = null) {
   Map formatted = respData.collectEntries() { [it.id, [name:it.name, imageUrl:it.imageUrl]] }
   logDebug("formatted response: ${prettyJson(formatted)}")
   formatted.each(){it ->
-    // logDebug("Favorite #${it.key}: ${it.value.name}")
     child.sendEvent(
       name: "Favorite #${it.key} ${it.value.name}",
       value: "<img src=\"${it.value.imageUrl}\" width=\"200\" height=\"200\" >",
@@ -584,55 +614,7 @@ void setRepeatMode(Map playModes) {
   }
 }
 
-void createGroupDevices() {
-  logDebug('Creating group devices...')
-  // deleteChildDevices()
-  state.userGroups.keySet().each{ it ->
-    String dni = "${app.id}-SonosGroupDevice-${it}"
-    DeviceWrapper device = getChildDevice(dni)
-    if (device == null) {
-      try {
-        logDebug "Creating group device for ${it}"
-        device = addChildDevice('hubitat', 'Generic Component Switch', dni, [name: 'Sonos Group', label: "Sonos Group: ${it}"])
-      } catch (UnknownDeviceTypeException e) {
-        logException 'createGroupDevices', e
-      }
-    }
-    String coordinatorId = state.userGroups["${it}"].coordinatorId as String
-    String playerIds =  state.userGroups["${it}"].playerIds as String
-    String householdId = state.players["${coordinatorId}"].householdId as String
-    device.updateDataValue('coordinatorId', coordinatorId)
-    device.updateDataValue('playerIds', playerIds)
-    device.updateDataValue('householdId', householdId)
 
-  }
-}
-
-void createPlayerDevices() {
-  List<Map> devicesToCreate = state.players.values().findAll { it -> it.id in playerDevices } ?: []
-  logDebug(devicesToCreate)
-  for (Map player in devicesToCreate) {
-    String dni = "${app.id}-${player.id}"
-    logDebug("DNI: ${dni}")
-    DeviceWrapper device = getChildDevice(dni)
-    if (device == null) {
-      try {
-        logInfo "Creating child audio notification device for ${player.name}"
-        device = addChildDevice('dwinks', 'Sonos Cloud Player', dni, [name: 'Sonos Cloud Player', label: "Sonos Cloud - ${player.name}"])
-      } catch (UnknownDeviceTypeException e) {
-        logException 'Sonos Cloud Player driver not found (needs installing?)', e
-      }
-    }
-    player.each { key, value -> if(key != 'zoneInfo') { device.updateDataValue(key, value as String) }}
-  }
-
-  for (ChildDeviceWrapper child in app.getChildDevices()) {
-    if(child.getDataValue('id') in playerDevices) { return } else {
-      logDebug("Removing child not found in selected devices list: ${child}")
-      app.deleteChildDevice(child.getDeviceNetworkId())
-    }
-  }
-}
 
 void setPlayModes(DeviceWrapper device, Map playModes) {
   logDebug 'Setting Play Modes...'
@@ -642,18 +624,79 @@ void setPlayModes(DeviceWrapper device, Map playModes) {
   postJsonAsync("${apiPrefix}/groups/${group}/playback/playMode", playModes)
 }
 
-void ungroupPlayer(DeviceWrapper device) {
-  logDebug 'Removing player from group...'
-  String householdId = device.getDataValue('householdId')
+void componentUngroupPlayer(DeviceWrapper device) {
+  logDebug('Removing player from group...')
   String playerId = device.getDataValue('id')
-  logDebug "DNI: ${playerId}"
-  Map data = ['playerIds': [playerId]]
+  getPlayersAndGroups([ungroupPlayer: true, playerId: playerId])
+}
+
+void ungroupPlayer(String playerId) {
+  logDebug("Ungroup Player: ${playerId}")
+  Map currentGroup = (state.groups.find{ it -> it.value.playerIds.contains(playerId)}).value
+  logDebug(prettyJson(currentGroup))
+  String coordinatorIdGroup = currentGroup.id.replace(':','%3A')
+  List playerIds = currentGroup.playerIds - playerId
+  Map data = ['playerIds': playerIds]
+  logDebug(prettyJson(data))
+  logDebug("${apiPrefix}/groups/${coordinatorIdGroup}/groups/setGroupMembers")
+  postJsonAsync("${apiPrefix}/groups/${coordinatorIdGroup}/groups/setGroupMembers", data)
+}
+
+void componentUngroupPlayers(DeviceWrapper device) {
+  logDebug('Removing players from group...')
+  String householdId = device.getDataValue('householdId')
+  List coordinatorId = [device.getDataValue('coordinatorId')]
+  List playerIds = device.getDataValue('playerIds').split(',')
+  List allPlayersIds = coordinatorId + playerIds
+  allPlayersIds.each(){ playerId ->
+    Map data = ['playerIds': [playerId]]
+    postJsonAsync("${apiPrefix}/households/${householdId}/groups/createGroup", data)
+  }
+}
+
+void componentGroupPlayers(DeviceWrapper device) {
+  logDebug('Adding players to group...')
+  String householdId = device.getDataValue('householdId')
+  List coordinatorId = [device.getDataValue('coordinatorId')]
+  List playerIds = device.getDataValue('playerIds').split(',')
+  logDebug("PlayerIds: ${playerIds}")
+  Map data = ['playerIds': coordinatorId + playerIds]
+  logDebug("Group Players Data: ${data}")
   postJsonAsync("${apiPrefix}/households/${householdId}/groups/createGroup", data)
+}
+
+void componentJoinPlayersToCoordinator(DeviceWrapper device) {
+  logDebug('Joining players to coordinator...')
+  String coordinatorId = device.getDataValue('coordinatorId')
+  String playerIds = device.getDataValue('playerIds')
+  getPlayersAndGroups([joinPlayers: true, coordinatorId: coordinatorId, playerIds: playerIds])
+}
+
+void componentRemovePlayersFromCoordinator(DeviceWrapper device) {
+  logDebug('Removing players from coordinator...')
+  String coordinatorId = device.getDataValue('coordinatorId')
+  String playerIds = device.getDataValue('playerIds')
+  getPlayersAndGroups([removePlayers: true, coordinatorId: coordinatorId, playerIds: playerIds])
+}
+
+void joinPlayers(String coordinatorId, String playerIdsJoined) {
+  logDebug("PlayerIds=: ${playerIdsJoined}")
+  String coordinatorIdGroup = ((state.groups.find{ it -> it.value.coordinatorId  == coordinatorId}).key).replace(':','%3A')
+  List playerIds = playerIdsJoined.split(',') as List
+  playerIds.add(coordinatorId)
+  Map data = ['playerIds': playerIds]
+  postJsonAsync("${apiPrefix}/groups/${coordinatorIdGroup}/groups/setGroupMembers", data)
+}
+
+void removePlayers(String coordinatorId) {
+  String coordinatorIdGroup = ((state.groups.find{ it -> it.value.coordinatorId  == coordinatorId}).key).replace(':','%3A')
+  Map data = ['playerIds': [coordinatorId]]
+  postJsonAsync("${apiPrefix}/groups/${coordinatorIdGroup}/groups/setGroupMembers", data)
 }
 
 void getHouseholds() {
   if(!state.householdsLastUpdated) { state.householdsLastUpdated = now() }
-  // if(now() - state.householdsLastUpdated < 1000 * 60 * 10) { return }
+  if(now() - state.householdsLastUpdated < 1000 * 60 * 2) { return } //Only update every 2 minutes
   Map params = [uri: "${apiPrefix}/households",headers: [authorization: "Bearer ${state.authToken}"]]
   sendQueryAsync(params, "getHouseholdsCallback")
   state.householdsLastUpdated = now()
@@ -673,40 +716,39 @@ void getHouseholdsCallback(AsyncResponse response, Map data = null) {
   state.households = households
 }
 
-void getPlayersAndGroups() {
-  if(!state.playersAndGroupsLastUpdated) { state.playersAndGroupsLastUpdated = now() }
-  // if(now() - state.playersAndGroupsLastUpdated < 1000 * 60 * 10) { return }
-  state.households.each{ householdId -> getPlayersAndGroupsAsync(householdId) }
-  state.playersAndGroupsLastUpdated = now()
+void getPlayersAndGroups(Map data = null) {
+  state.households.each{ householdId -> getPlayersAndGroupsAsync(householdId, data) }
 }
 
-void getPlayersAndGroupsAsync(String householdId) {
-  logDebug "Requesting players and groups for ${householdId}"
+void getPlayersAndGroupsAsync(String householdId, Map data = null) {
+  logDebug("Requesting players and groups for ${householdId}")
   Map params = [
     uri: "${apiPrefix}/households/${householdId}/groups",
     headers: [ authorization: 'Bearer ' + state.authToken, contentType: 'application/json' ]
   ]
-  sendQueryAsync(params, "getPlayersAndGroupsCallback", [householdId: householdId])
+  data = data != null ? data + [householdId: householdId] : [householdId: householdId]
+  sendQueryAsync(params, "getPlayersAndGroupsCallback", data)
 }
 
 void getPlayersAndGroupsCallback(AsyncResponse response, Map data = null) {
-  logDebug("Data: ${data}")
+  logDebug("getPlayersAndGroupsCallback Data: ${data}")
   if(response.hasError()) {
     logError("getPlayers error: ${response.getErrorData()}")
     return
   }
 
-  Map<String, List<Map>> result =[:]
-  logDebug("getPlayers request response: ${prettyJson(response.getJson())}")
-  result = response.getJson()
+  Map<String, List<Map>> result = response.getJson()
+  logDebug("getPlayers result: ${prettyJson(result)}")
   Map household = [householdId: data.householdId]
   result.players.collectEntries { p -> [(p.id): p + household] }
   result.groups.collectEntries { g -> [(g.id): g + household] }
 
   state.players = result.players.collectEntries { p -> [(p.id): p + household] }
   logDebug("Players: ${prettyJson(state.players)}")
-  // state.groups = result.groups.findAll{ it -> it.playerIds.size() > 1}
   state.groups = result.groups.collectEntries { g -> [(g.id): g + household] }
+  if(data.containsKey(joinPlayers)) { joinPlayers(data.coordinatorId, data.playerIds) }
+  if(data.containsKey(removePlayers)) { removePlayers(data.coordinatorId) }
+  if(data.containsKey(ungroupPlayer)) { ungroupPlayer(data.playerId) }
 }
 
 String getGroupForPlayer(String playerId) {
@@ -772,18 +814,12 @@ void sendQueryAsync(Map params, String callback, Map data = null) {
 }
 
 void postResponse(AsyncResponse response, Map data) {
+  if (response.status != 200) {
+    logError("post request returned HTTP status ${response.status}")
+  }
   if (response.hasError()) {
-    logError "post request error: ${response.getErrorMessage()}"
-  } else if (response.status != 200) {
-    logError "post request returned HTTP status ${response.status}"
+    logError("post request error: ${response.getErrorMessage()}")
   } else {
-    logDebug "postResponse: ${response.data}"
+    logDebug("postResponse: ${response.data}")
   }
 }
-
-void eventsCallback() {
-  logDebug("${app.getLabel()} executing 'webhook()'")
-  logDebug("params: $params")
-  logDebug("request: $request")
-}
-
