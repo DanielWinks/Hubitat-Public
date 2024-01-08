a4a45dbb74ccb4c1adad48016f9795c94fb90f4a
Daniel Winks <daniel.winks@gmail.com>
2024-01-07 19:09:02 -0500

    Update simple_notify.py

--- simple_notify.py
+++ simple_notify.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import request
 import sys
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
 @app.route('/notify', methods=['NOTIFY'])
 def log():
-    sys.stdout.write('Body: {}\r\n'.format(request.headers))
+    root = ET.fromstring(request.get_data())
+    ET.indent(root)
+    sys.stdout.write('Headers: {}\r\n'.format(request.headers))
     sys.stdout.flush()
-    sys.stdout.write('Body: {}\r\n'.format(request.get_data()))
+    sys.stdout.write('Body: {}\r\n'.format(ET.tostring(root, encoding='unicode')))
     sys.stdout.flush()
     return '200: OK'
 
