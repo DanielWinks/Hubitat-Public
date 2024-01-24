c35195267f96a720f659faa60471bd1ecb93f5a8
Daniel Winks <daniel.winks@gmail.com>
2024-01-23 20:45:46 -0500

    Update SonosAdvancedApp.groovy

--- Apps/SonosAdvancedApp.groovy
+++ Apps/SonosAdvancedApp.groovy
@@ -28,7 +28,7 @@ import com.hubitat.hub.domain.Location
 
 definition(
   name: 'Sonos Advanced Controller',
-  version: '0.3.21',
+  version: '0.3.22',
   namespace: 'dwinks',
   author: 'Daniel Winks',
   category: 'Audio',
