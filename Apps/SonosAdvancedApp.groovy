4e639747b1da9c3e4afe06ae99e1eb2f5f9091c0
Daniel Winks <daniel.winks@gmail.com>
2024-02-07 16:59:19 -0500

    0.4.4

--- Apps/SonosAdvancedApp.groovy
+++ Apps/SonosAdvancedApp.groovy
@@ -26,7 +26,7 @@
 
 definition(
   name: 'Sonos Advanced Controller',
-  version: '0.4.3',
+  version: '0.4.4',
   namespace: 'dwinks',
   author: 'Daniel Winks',
   category: 'Audio',
--- Drivers/Component/SonosAdvPlayer.groovy
+++ Drivers/Component/SonosAdvPlayer.groovy
@@ -27,7 +27,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Player',
-    version: '0.4.3',
+    version: '0.4.4',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     singleThreaded: true,
