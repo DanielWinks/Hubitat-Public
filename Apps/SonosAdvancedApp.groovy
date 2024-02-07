549a8dcfd0f11d0e9a72d393d1b4a34d90de78e9
Daniel Winks <daniel.winks@gmail.com>
2024-02-06 19:10:16 -0500

    v0.4.3 - Improved WS connectivity checks

--- Apps/SonosAdvancedApp.groovy
+++ Apps/SonosAdvancedApp.groovy
@@ -26,7 +26,7 @@
 
 definition(
   name: 'Sonos Advanced Controller',
-  version: '0.4.2',
+  version: '0.4.3',
   namespace: 'dwinks',
   author: 'Daniel Winks',
   category: 'Audio',
--- Drivers/Component/SonosAdvFavorites.groovy
+++ Drivers/Component/SonosAdvFavorites.groovy
@@ -26,7 +26,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Favorites',
-    version: '0.4.2',
+    version: '0.4.3',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     component: true,
