49767977723ec307c1ee513323312eae98e688d0
Daniel Winks <daniel.winks@gmail.com>
2024-02-22 20:00:31 -0500

    Fix stackoverflow error on load fav

--- Drivers/Component/SonosAdvFavorites.groovy
+++ Drivers/Component/SonosAdvFavorites.groovy
@@ -26,7 +26,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Favorites',
-    version: '0.5.0',
+    version: '0.5.1',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     component: true,
