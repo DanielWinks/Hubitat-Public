eef66314abbdb44405e53d1a20f0ade1e40f9095
Daniel Winks <daniel.winks@gmail.com>
2025-01-14 19:13:31 -0500

    Fix MRVAT_ should be MRRC_

--- Apps/SonosAdvancedApp.groovy
+++ Apps/SonosAdvancedApp.groovy
@@ -26,7 +26,7 @@
 
 definition(
   name: 'Sonos Advanced Controller',
-  version: '0.7.3',
+  version: '0.7.4',
   namespace: 'dwinks',
   author: 'Daniel Winks',
   category: 'Audio',
--- Drivers/Component/SonosAdvPlayer.groovy
+++ Drivers/Component/SonosAdvPlayer.groovy
@@ -27,7 +27,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Player',
-    version: '0.7.3',
+    version: '0.7.4',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     singleThreaded: false,
