8f575b7303e799ff5d8c58c1e498d1c638a54e51
Daniel Winks <daniel.winks@gmail.com>
2026-01-31 12:43:12 -0500

    Add release workflow for Gemini Text Rewriter and set initial version to 1.0.0

--- Apps/GeminiTextRewriter.groovy
+++ Apps/GeminiTextRewriter.groovy
@@ -54,6 +54,7 @@ import groovy.transform.Field
  */
 definition(
   name: 'Gemini Text Rewriter',
+  version: '1.0.0',
   namespace: 'dwinks',
   author: 'Daniel Winks',
   description: 'Use Google Gemini API to rewrite text snippets with various styles.',
