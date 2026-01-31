0d77304f8559ce7757feb2895edd1d00e21fc7fb
Daniel Winks <daniel.winks@gmail.com>
2026-01-31 11:24:34 -0500

    Refactor player names collection to ensure type safety and improve clarity

--- Drivers/Component/SonosAdvPlayer.groovy
+++ Drivers/Component/SonosAdvPlayer.groovy
@@ -3611,8 +3611,8 @@ void processWebsocketMessage(String message) {
       if(coordinatorName != null && coordinatorName != '') {setGroupCoordinatorName(coordinatorName)}
 
       try {
-        List<String> playerNames = group.playerIds.collect{pid -> players.find{player-> player?.id == pid}?.name}
-        List<String> groupMemberNames = (ArrayList<String>)(playerNames.findAll{it != null})
+        List<String> playerNames = (List<String>)group.playerIds.collect{pid -> players.find{player-> player?.id == pid}?.name as String}
+        List<String> groupMemberNames = playerNames.findAll{it != null}
         setGroupMemberNames(groupMemberNames)
       } catch (Exception e) {logTrace('Could not get group member names, continuing on...')}
 
