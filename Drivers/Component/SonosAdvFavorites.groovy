992cc187b68ade864f5914d3f7b5cf5a94d0f805
DanielWinks <DanielWinks@users.noreply.github.com>
2026-02-01 17:10:47 +0000

    Release Sonos Advanced Controller 0.7.12

--- Drivers/Component/SonosAdvFavorites.groovy
+++ Drivers/Component/SonosAdvFavorites.groovy
@@ -26,7 +26,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Favorites',
-    version: '0.7.11',
+    version: '0.7.12',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     component: true,
