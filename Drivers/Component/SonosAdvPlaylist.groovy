15b645b47a0d8c5b6c87899db7eb9dde3eec0d7f
Daniel Winks <daniel.winks@gmail.com>
2026-02-18 06:45:59 -0500

    Update SonosAdvPlaylist.groovy

--- Drivers/Component/SonosAdvPlaylist.groovy
+++ Drivers/Component/SonosAdvPlaylist.groovy
@@ -26,7 +26,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Playlist',
-    version: '0.10.1',
+    version: '0.10.3',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     component: true,
