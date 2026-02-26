0bd35d5f7923905e698e229f738f6428b4a6af4c
DanielWinks <DanielWinks@users.noreply.github.com>
2026-02-26 15:11:43 +0000

    Release Sonos Advanced Controller 0.10.11

--- Drivers/Component/SonosAdvPlayer.groovy
+++ Drivers/Component/SonosAdvPlayer.groovy
@@ -27,7 +27,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Player',
-    version: '0.10.10',
+    version: '0.10.11',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     singleThreaded: false,
--- Drivers/Component/SonosAdvSnapshot.groovy
+++ Drivers/Component/SonosAdvSnapshot.groovy
@@ -26,7 +26,7 @@
 metadata {
   definition(
     name: 'Sonos Advanced Snapshot',
-    version: '0.10.10',
+    version: '0.10.11',
     namespace: 'dwinks',
     author: 'Daniel Winks',
     component: true,
